1ff26c5fa9e8d3b6c9d0b8ade3fbef49ed1026f5
Richard Loera <richexp9@gmx.com>
2017-12-31 10:54:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,10 +25,9 @@ def won?(board)
       return combination # return the win_combination indexes that won.
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return combination
-    else
-      false
     end
   end
+  false
 end
 
 def full?(board)
